a4248650e46e22f0fc6a31bff5e7c14b3d96bea5
Eric Wise <ericwise@gmail.com>
2021-06-23 21:20:10 -0500

    working

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import './App.css'
 import Posts from './components/Posts';
+import Pagination from './components/Pagination';
 
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage, setPostsPerPage] = useState(10);
+  const [postsPerPage ] = useState(10);
 
   useEffect(()=> {
     const fetchPosts = async () => {
@@ -24,13 +25,20 @@ function App() {
  const indexOfFirstPost = indexOfLastPost - postsPerPage;
  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost)
 
+ const paginate = pageNumber => setCurrentPage(pageNumber);
 
-  return (
-    <div className="container mt-5">
-      <h1 className="text-primary mb-3">Paginate</h1>
-      <Posts posts={currentPosts} loading={loading} />
-    </div>
-  );
-}
+
+ return (
+  <div className='container mt-5'>
+    <h1 className='text-primary mb-3'>Paginate</h1>
+    <Posts posts={currentPosts} loading={loading} />
+    <Pagination
+      postsPerPage={postsPerPage}
+      totalPosts={posts.length}
+      paginate={paginate}
+    />
+  </div>
+);
+};
 
 export default App;
