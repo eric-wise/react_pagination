3e03356946ce38f2def74afe3caf1eea0b7f2fba
Eric Wise <ericwise@gmail.com>
2021-06-23 20:45:41 -0500

    First Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import './App.css'
+import Posts from './components/Posts';
 
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+
+  useEffect(()=> {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      setPosts(res.data)
+      setLoading(false)
+    }
+    fetchPosts();
+  },[]);
+
+ const indexOfLastPost = currentPage * postsPerPage;
+ const indexOfFirstPost = indexOfLastPost - postsPerPage;
+ const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost)
+
+
   return (
-    <div className="App">
-      <h1>App</h1>
+    <div className="container mt-5">
+      <h1 className="text-primary mb-3">Paginate</h1>
+      <Posts posts={currentPosts} loading={loading} />
     </div>
   );
 }
